de85be8619e34dc6170eb41f7429804ad530c3dd
Aaron <aronchj7@live.com>
2021-04-29 21:42:51 -0600

    Fixing Adapter Issues

--- app/src/main/java/com/example/pokedex/adapter/PokemonCardAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter/PokemonCardAdapter.kt
@@ -1,54 +1,32 @@
 package com.example.pokedex.adapter
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.RecyclerView
-//import com.bumptech.glide.Glide
-import com.example.pokedex.R
-import com.example.pokedex.models.Pokemon
 
-class PokemonCardAdapter: RecyclerView.Adapter<PokemonCardAdapter.PokemonCardViewHolder>()  {
-    //TODO Paso 5, creamos el set para la lista que va a alimentar el adapter (datasource)
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex.databinding.PokemonCardBinding
+import com.example.pokedex.models.Pokemon
+import com.squareup.picasso.Picasso
+
+class PokemonCardAdapter : RecyclerView.Adapter<PokemonCardAdapter.MyViewHolder>() {
+
+    class MyViewHolder(val binding: PokemonCardBinding) : RecyclerView.ViewHolder(binding.root)
+
     var pokemons: List<Pokemon> = emptyList()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    //    TODO Paso 3, Creamos el ViewHolder
-    inner class PokemonCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(pokemon: Pokemon) {
-            itemView.textView2.text = pokemon.name
-
-            // TODO Click listener de la celda
-//            itemView.setOnClickListener {
-//                itemView.textView2.visibility = itemView.textView2.isVisible.not().mapToVisibility()
-//            }
-
-//            Picasso.get().load(imageUrl).into(itemView.imageView)
-//            Glide.with(itemView.context)
-//                    .load(imageUrl)
-//                    .circleCrop()
-//                    .into(itemView.imageView)
-//
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(PokemonCardBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    //TODO Metodo encargado de inflar el layout/xml en cada celda
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonCardViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_card, parent, false)
-        return PokemonCardViewHolder(view)
-    }
-
-    // TODO Metodo encargado de pintar los datos para cada celda, dependiendo de la posicion
-    override fun onBindViewHolder(holder: PokemonCardViewHolder, position: Int) {
-        // Llamamos al metodo bind para la posicion especifica
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val pokemon = pokemons[position]
-        holder.bind(pokemon)
+        Picasso.get().load("https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png").into(holder.binding.imageView);
+        holder.binding.textView.text = pokemon.name
     }
 
-    // TODO Metodo que determina la cantidad de elementos en la lista
     override fun getItemCount(): Int = pokemons.size
 
 }
