8223a7d7a23117675eb54fa815134ac3ce022aa5
Aaron <aronchj7@live.com>
2021-05-09 17:18:49 -0600

    Updating Empty State

--- app/src/main/java/com/example/pokedex/widgets/EmptyInfoView.kt
+++ app/src/main/java/com/example/pokedex/widgets/EmptyInfoView.kt
@@ -5,15 +5,15 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.pokedex.R
-import com.example.pokedex.databinding.EmptyViewBinding
+import com.example.pokedex.databinding.FragmentEmptyStateBinding
 
 class EmptyInfoView  @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr){
-    private lateinit var binding: EmptyViewBinding
+    private var binding: FragmentEmptyStateBinding =
+        FragmentEmptyStateBinding.inflate( LayoutInflater.from(context), this, true)
 
     init {
-        binding = EmptyViewBinding.inflate( LayoutInflater.from(context), this, true)
         attrs.let {
             val typedArray = context.theme.obtainStyledAttributes(
                     attrs,
@@ -21,7 +21,9 @@ class EmptyInfoView  @JvmOverloads constructor(
                     defStyleAttr,
                     defStyleRes
             )
-            binding.emptyInfoText.text = typedArray.getString(R.styleable.EmptyInfoView_infoText)
+            binding.title.text = typedArray.getString(R.styleable.EmptyInfoView_title)
+            binding.subtitle.text = typedArray.getString(R.styleable.EmptyInfoView_subtitle)
+            binding.button.text = typedArray.getString(R.styleable.EmptyInfoView_button)
             typedArray.recycle()
         }
     }
