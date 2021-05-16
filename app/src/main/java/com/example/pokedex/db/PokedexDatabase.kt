12f38cf64662d83621116c5085896c2d01654299
Roy Solera <sq16roy@gmail.com>
2021-05-16 16:54:11 -0600

    repository

--- app/src/main/java/com/example/pokedex/db/PokedexDatabase.kt
+++ app/src/main/java/com/example/pokedex/db/PokedexDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.pokedex.db.dao.UserDao
+import com.example.pokedex.db.entities.Favorite
 import com.example.pokedex.db.entities.User
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class, Favorite::class], version = 1)
 abstract class PokedexDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao?
 
--- app/src/main/java/com/example/pokedex/db/entities/User.kt
+++ app/src/main/java/com/example/pokedex/db/entities/User.kt
@@ -12,11 +12,3 @@ data class User(
 ) {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 }
-//@Entity(tableName = "users")
-//class User {
-//    @PrimaryKey(autoGenerate = true)
-//    var id: Int? = null
-//
-//    @ColumnInfo(name = "userId")
-//    var userId: String? = null
-//}
